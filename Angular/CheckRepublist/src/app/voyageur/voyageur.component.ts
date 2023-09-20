6d0b16600046b29d11303391e76ba00f3aa9415b
Tidiane23 <tidianediallo426@gmail.com>
2023-09-20 09:24:10 +0200

    création d'un projet angular

---
+++ Angular/CheckRepublist/src/app/voyageur/voyageur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-voyageur',
+  templateUrl: './voyageur.component.html',
+  styleUrls: ['./voyageur.component.css']
+})
+export class VoyageurComponent {
+
+}
