b30920da23872dd48d1d37cd74b455947313c451
Tidiane DIALLO <tidianediallo426@gmail.com>
2023-09-21 16:34:55 +0200

    materielref+critere+correction

---
+++ CheckRepubList/src/app/critere/critere-http.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Critere } from '../model';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CritereHttpService {
+  criteres: Array<Critere> = new Array<Critere>();
+
+  apiCritereUrl: string = environment.apiUrl + "/critere";
+
+  constructor(private http: HttpClient) {
+    this.load();
+   }
+
+   load(): void{
+    let obs: Observable<Critere[]> = this.http.get<Critere[]>(this.apiCritereUrl);
+
+    obs.subscribe(response => {
+      this.criteres = response;
+    });
+   }
+
+   findAll() : Array<Critere> {
+    return this.criteres;
+  }
+  findAllForAsync(): Observable<Critere[]> {
+    return this.http.get<Critere[]>(this.apiCritereUrl);
+  } 
+
+  findById(id: number): Observable<Critere> {
+    let obs: Observable<Critere> = this.http.get<Critere>(this.apiCritereUrl + "/"+id);
+
+    return obs;
+  }
+
+  save(critere: Critere): void {
+    if(critere.id) { // mise à jour
+      this.http.put<Critere>(this.apiCritereUrl + "/"+critere.id, critere).subscribe(response => {
+        this.load();
+      });
+    } else { // création
+      this.http.post<Critere>(this.apiCritereUrl, critere).subscribe(response => {
+        this.load();
+      });
+    }
+   }
+   deleteById(id: number) {
+    this.http.delete<void>(this.apiCritereUrl + "/"+id).subscribe(response => {
+      this.load();
+    });
+   }
+}
+
+
--- CheckRepubList/src/app/critere/critere.component.ts
+++ CheckRepubList/src/app/critere/critere.component.ts
@@ -1,38 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Critere } from '../model';
 import { CritereService } from './critere.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CritereHttpService } from './critere-http.service';
 
 @Component({
   selector: 'app-critere',
   templateUrl: './critere.component.html',
   styleUrls: ['./critere.component.scss']
 })
-export class CritereComponent {
+export class CritereComponent implements OnInit {
 
-  critereForm: Critere = null;
- 
+  //critereForm: Critere = null;
+  critereForm: FormGroup;
+  showForm: boolean  =false;
 
-  constructor(private critereService: CritereService){}
+  constructor(private critereHttpService: CritereHttpService, private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.  critereForm = this.formBuilder.group({
+      id: this.formBuilder.control(0),
+      materielRef: this.formBuilder.control(Validators.required),
+      logement: this.formBuilder.control(''),
+      deplacement: this.formBuilder.control(''),
+      climat: this.formBuilder.control('')
+    });
+  }
 
  list(): Array<Critere>{
 
-  return this.critereService.findAll();
+  return this.critereHttpService.findAll();
 
  }
 
  add() {
-  this.critereForm = new Critere();
+  this.critereForm.reset();
+  this.showForm = true;
 }
 edit(id: number) {
-  this.critereForm = {...this.critereService.findById(id)};
+  this.critereHttpService.findById(id).subscribe(response => {
+    this.critereForm.patchValue(response);
+    this.showForm = true;
+  });
 }
+
 remove(id: number) {
-  this.critereService.deleteById(id);
+  this.critereHttpService.deleteById(id);
 }
 save() {
-  this.critereService.save(this.critereForm);
+  this.critereHttpService.save(this.critereForm.value);
 }
+
 cancel() {
-  this.critereForm = null;
+  this.critereForm.reset();
+  this.showForm = false;
 }
 }
