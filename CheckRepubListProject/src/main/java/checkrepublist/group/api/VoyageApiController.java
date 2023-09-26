57e3abf6f10a100184b5573af39206bfad6e72b5
AnaisP713 <anais.portet@gmail.com>
2023-09-26 10:56:20 +0200

    Ajout enum role+apicontroller+classcompte

--- CheckRepubListProject/src/main/java/checkrepublist/group/api/VoyageApiController.java
+++ CheckRepubListProject/src/main/java/checkrepublist/group/api/VoyageApiController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
 import checkrepublist.group.api.request.VoyageRequest;
 import checkrepublist.group.api.response.VoyageResponse;
 import checkrepublist.group.dao.IDAOCritere;
